fda810a1d004a9dcf7b2029b5ab33b214f52389d
angler <2926686459@qq.com>
2024-11-18 10:11:32 +0800

    feat：单测 和 icon组件

--- calendar-component/src/App.tsx
+++ calendar-component/src/App.tsx
@@ -3,15 +3,10 @@ import Calendar from "./Calendar";
 import { useState } from "react";
 
 function App() {
-  const [value, setValue] = useState(dayjs("2023-11-20"));
 
   return (
     <div className="App">
       <Calendar
-        value={value}
-        onChange={(val) => {
-          setValue(val);
-        }}
       ></Calendar>
     </div>
   );
