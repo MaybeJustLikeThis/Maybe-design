bd09069a7f46fdda5f5d593b94b2423dd35cf3b0
angler <2926686459@qq.com>
2024-12-30 20:21:42 +0800

    feat：messge组件开发

---
+++ message-component/src/Message/index.tsx
@@ -0,0 +1,126 @@
+import {
+  CSSProperties,
+  FC,
+  ReactNode,
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  createRef,
+  useRef,
+} from "react";
+import useStore, { MessageList } from "./useStore";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+import "./index.scss";
+import { createPortal } from "react-dom";
+import { useTimer } from "./useTimer";
+
+export type Position = "top" | "bottom";
+
+export interface MessageProps {
+  style?: CSSProperties;
+  className?: string | string[];
+  content: ReactNode | string;
+  duration?: number;
+  onClose?: (...args: any) => void;
+  id?: number;
+  position?: Position;
+  
+}
+
+const MessageItem: FC<MessageProps> = (item) => {
+
+
+
+  const { onMouseEnter, onMouseLeave } = useTimer({
+    id: item.id!,
+    duration: item.duration,
+    remove: item.onClose!,
+  });
+
+  return (
+    <div
+
+      className="message-item"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {item.content}
+    </div>
+  );
+};
+
+export interface MessageRef {
+  add: (messageProps: MessageProps) => number;
+  remove: (id: number) => void;
+  update: (id: number, messageProps: MessageProps) => void;
+  clearAll: () => void;
+}
+
+export const MessageProvider = forwardRef<MessageRef, {}>((props, ref) => {
+  const { messageList, add, update, remove, clearAll } = useStore("top");
+
+  //   useImperativeHandle(//他的执行时机有问题，不是立即修改ref
+  //     ref,
+  //     () => {
+  //       return {
+  //         add,
+  //         update,
+  //         remove,
+  //         clearAll,
+  //       };
+  //     },
+  //     []
+  //   );
+
+  if ("current" in ref!) {
+    ref.current = {
+      add,
+      update,
+      remove,
+      clearAll,
+    };
+  }
+
+  const positions = Object.keys(messageList) as Position[];
+
+  const messageWrapper = (
+    <div className="message-wrapper">
+      {positions.map((direction) => {
+        return (
+          <div className={`message-wrapper-${direction}`} key={direction}>
+            <TransitionGroup>
+              {messageList[direction].map((item) => {
+                const nodeRef = createRef<HTMLDivElement>();
+                return (
+                  <CSSTransition
+                    key={item.id}
+                    timeout={1000}
+                    classNames="message"
+                    nodeRef={nodeRef}
+                  >
+                    <div ref={nodeRef}>
+                      {/* 解决报错 findDOMNode */}
+                      <MessageItem onClose={remove} {...item} />
+                    </div>
+                  </CSSTransition>
+                );
+              })}
+            </TransitionGroup>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  const el = useMemo(() => {
+    const el = document.createElement("div");
+    el.className = `wrapper`;
+
+    document.body.appendChild(el);
+    return el;
+  }, []);
+
+  return createPortal(messageWrapper, el);
+});
